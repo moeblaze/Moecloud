02d0ca54d5aef478094eeb1fc076ca0a245b93c6
Morris Stephens <46266209+moeblaze@users.noreply.github.com>
2025-09-15 11:38:12 -0400

    Add files via upload

--- assets/js/scripts.js
+++
@@ -1,6 +0,0 @@
-
-// Minimal progressive enhancements
-document.querySelectorAll('a[data-active]').forEach(a=>{
-  if (location.pathname.endsWith(a.getAttribute('href'))) a.classList.add('active');
-});
-// Lazy load images (native loading attr already used)
