eee16e3d97988c32ef275f71117002fd874a6868
Morris Stephens <46266209+moeblaze@users.noreply.github.com>
2025-09-26 14:45:11 -0400

    Add files via upload

---
+++ downloads/policy-pack/logging.tf
@@ -0,0 +1,8 @@
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "org-logs-bucket"
+  versioning { enabled = true }
+  server_side_encryption_configuration {
+    rule { apply_server_side_encryption_by_default { sse_algorithm = "AES256" } }
+  }
+}
